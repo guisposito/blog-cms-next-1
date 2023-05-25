32a9b4099fe80b00ff7a1cb54cb50662384cccbd
guisposito <guicarlosjoinhas@gmail.com>
2023-05-25 20:35:31 -0300

    Adicionado o global scss

--- blog-cms/pages/index.js
+++ blog-cms/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { PostCard, Categories, PostWidget} from '../components/Index'
 
 const posts = [
   {title: "React Testing", excerpt: "Learn React testing"},
@@ -9,7 +8,7 @@ const posts = [
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-green-300">
+    <div className="container mx-auto px-10 mb-8 bg-gray-300">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -17,15 +16,13 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <div className="">
-              {post.title}
-              {post.excerpt}
-            </div>
+              <PostCard post={post} key={post.title}/>
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-
+              <PostWidget />
+              <Categories />
             </div>
         </div>
       </div>
