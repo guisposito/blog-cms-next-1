187ca2e8e243667cb616fa8a88e2b192221cb981
guisposito <guicarlosjoinhas@gmail.com>
2023-05-28 16:58:28 -0300

    Terminado o card de posts

--- blog-cms/components/PostCard.jsx
+++ blog-cms/components/PostCard.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import moment from 'moment';
-import Link from 'next/link';
+import React from "react";
+import moment from "moment";
+import Link from "next/link";
 
 const PostCard = ({ post }) => {
   console.log(post);
@@ -44,10 +44,19 @@ const PostCard = ({ post }) => {
               d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
             />
           </svg>
+          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
         </div>
+      </div>
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+        {post.excerpt}
+      </p>
+      <div className="text-center">
+        <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+          <Link href={`/post/${post.slug}`}>Continue Reading</Link>
+        </span>
       </div>
     </div>
   );
-}
+};
 
-export default PostCard
+export default PostCard;
