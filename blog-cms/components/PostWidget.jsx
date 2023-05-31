484fa388b71db33434c618842f86a59535261696
guisposito <guicarlosjoinhas@gmail.com>
2023-05-30 22:28:17 -0300

    Feito o fetch dos widget de posts

--- blog-cms/components/PostWidget.jsx
+++ blog-cms/components/PostWidget.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import moment from 'moment';
+import Link from 'next/link'
+import { getRecentPosts, getSimilarPosts } from '../services';
 
-const PostWidget = () => {
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+  
+  useEffect(() => {
+    if(slug){ 
+        getSimilarPosts(category, slug).then((result) =>setRelatedPosts(result))
+    } else {
+        getRecentPosts().then((result) => setRelatedPosts(result))
+    }
+  }, [slug])
+  
+  console.log(relatedPosts)
   return (
     <div>
-      PostWidget
+      
     </div>
   )
 }
