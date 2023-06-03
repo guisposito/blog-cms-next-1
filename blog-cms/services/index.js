8ecfce8387c115b7aa12c349fc0d6342fb8dfd27
guisposito <guicarlosjoinhas@gmail.com>
2023-06-02 22:33:47 -0300

    post slug corrigido

--- blog-cms/services/index.js
+++ blog-cms/services/index.js
@@ -58,12 +58,12 @@ export const getRecentPosts = async () => {
   return result.posts;
 };
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
         where: {
-          slug_not: $slug
+          slug_not: $slug,
           AND: { categories_some: { slug_in: $categories } }
         }
         last: 3
@@ -77,11 +77,10 @@ export const getSimilarPosts = async () => {
       }
     }
   `;
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query,{ categories, slug });
 
   return result.posts;
 };
-
 
 export const getCategories = async () => {
   const query = gql`
@@ -91,8 +90,42 @@ export const getCategories = async () => {
         slug
       }
     }
-  `
+  `;
   const result = await request(graphqlAPI, query);
 
   return result.categories;
-}
+};
+
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content{
+          raw
+        }
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.post;
+};
